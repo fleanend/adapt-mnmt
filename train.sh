316edba132cdd94cc1d9da60fe5d10e43d8aa329
Edoardo Ferrante <edoardoferrante@hotmail.it>
2020-09-16 14:35:52 +0200

    fix train.sh

--- train.sh
+++ train.sh
@@ -3,30 +3,31 @@
 #EXPATH=$1 	# ./models/pt-en
 EXPID=$1	# pt-en
 DEVICES=$2	# -1 for cpu 
+RUNDIR=$3
+DATADIR=$4		# preprocessed data from ./scripts/preprocess.sh [exp-id] [subword-size]
+
 export CUDA_VISIBLE_DEVICES=$DEVICES
-EXPDIR=$PWD
+EXPDIR="/content/adapt-mnmt/"
 
 ONMT=$EXPDIR/OpenNMT/opennmt
 CONFIG=$EXPDIR/config.yml
 MODEL_DEFN=$EXPDIR/model_defn.py	# update model capacity, based on single pair or multilingual model training
 
-RUNDIR=$EXPDIR/models/$EXPID 		# dir where exp is run 
-DATADIR=$RUNDIR/data/spdata 		# preprocessed data from ./scripts/preprocess.sh [exp-id] [subword-size]
 LOG=$RUNDIR/log.train
 
 
-if [ ! -d $RUNDIR/model ]; then
+if [[ ! -d $RUNDIR/model ]]; then
 
-  mkdir -p $RUNDIR
-  echo -e "\nTRAINING LOG: [ $LOG ]"
+  #mkdir -p $RUNDIR
+  #echo -e "\nTRAINING LOG: [ $LOG ]"
   
   python $ONMT/bin/main.py train_and_eval \
 			--config $CONFIG \
 			--model $MODEL_DEFN \
-			--run_dir $RUNDIR 
-			--data_dir $DATADIR \
+			--run_dir "$RUNDIR" \
+			--data_dir "$DATADIR" \
 			--seed 1234 \
-			--num_gpus 2 > $LOG 2> $LOG
+			--num_gpus 2
 
 			#--auto_config \
 			#--checkpoint_path parent_model \
